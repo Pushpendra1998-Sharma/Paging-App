d6ac5dcc3bca19d77f0e846acfcaf3b677164b92
Pushpendra1998-23 <pushpendra.19m143022@abes.ac.in>
2021-06-20 20:30:01 +0530

    Latest

---
+++ src/component/App.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+
+import Pagination from './Pagination';
+
+import './App.css';
+
+
+
+export default function App() {
+
+  const [users_list, setusers_list] = useState([]);
+  const [response_data, setresponse_data] = useState(null);
+  const [no_on_page, setno_on_page] = useState(1);
+  const [per_page_item, setper_page_item] = useState(null);
+  const [total_page_item, settotal_page_item] = useState(null);
+  const [total_page, settotal_page] = useState(null);
+
+  const fetchData = () => {
+    return fetch(`https://reqres.in/api/users?page=${no_on_page}`)
+          .then((response) => response.json())
+          .then((data) =>
+            // setusers_list([...data.data])
+            { setresponse_data(data);
+              setusers_list([...data.data]);
+              setno_on_page(data.page);
+              setper_page_item(data.per_page);
+              settotal_page_item(data.total);
+              settotal_page(data.total_pages);
+            });
+        }
+console.log("Response Data",response_data);
+console.log("User Data",users_list);
+console.log("Pages", no_on_page)
+
+
+const paginate = (pageNumber) => {
+  setno_on_page(pageNumber);
+}
+
+useEffect(() => {
+  if (no_on_page)
+  fetchData();
+}, [no_on_page]);
+
+
+  
+  useEffect(() => {
+    fetchData();
+    }, []);
+
+    return (
+        <div className="page-design">
+          <table className="table-size">
+          <tr>
+              <th>Id</th>
+            <th>E-mail</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+          {users_list.map((items, index) => { 
+             return <tr>
+                  <td>{items.id}</td>
+                 <td>{items.email}</td>
+                 <td> {items.first_name} </td>
+                 <td> {items.last_name} </td>   
+              </tr>
+          })}
+          </table>
+          <Pagination
+            per_page_item={per_page_item}
+            total_page={total_page}
+            paginate={paginate}
+          />
+        </div>
+    )
+}
+
+
+
+
+
---
+++ src/component/Pagination.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Pagination  ({ per_page_item, total_page, paginate })  {
+const [pageNumbers1, setpageNumbers] = useState([]);
+
+for (let i = 1; i < total_page + 1; i++) {
+    pageNumbers1.push(i);
+  }
+
+if (pageNumbers1.length > total_page)
+{
+    pageNumbers1.splice(total_page, pageNumbers1.length)
+}
+
+ console.log("Page numbers paging", pageNumbers1)
+    return (
+        <div>
+             <nav>
+            <ul className='pagination'>
+                {pageNumbers1.map(number => (
+                <li key={number} className='page-item'>
+                    <a onClick={() => paginate(number)} href='!#' className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+            
+        </div>
+    )
+}
+
+export default Pagination;
